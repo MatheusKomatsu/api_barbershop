1f61bed2d0a00126ce833fea9e778628297fa037
Douglas Nicida <douglasnicida@gmail.com>
2024-06-15 23:25:36 -0300

    [fix] - datetime zone

--- src/main/java/br/unesp/barbershop/api/ApiApplication.java
+++ src/main/java/br/unesp/barbershop/api/ApiApplication.java
@@ -1,18 +1,17 @@
 package br.unesp.barbershop.api;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -27,6 +26,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 public class ApiApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
